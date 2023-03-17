67b84fc18b40f942e455ee1e9cd1b2b8ecc582e2
Xac47 <hasbulat69@gmail.com>
2023-03-17 18:43:17 +0300

    update file blog/admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -3,8 +3,24 @@ from mptt.admin import MPTTModelAdmin
 
 from . import models
 
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'auther', 'create_at', 'id')
+    ordering = ('-id',)
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'prep_time', 'cook_time', 'post')
+    ordering = ('-id',)
+
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('message', 'name', 'email', 'post')
+    ordering = ('-id',)
+
+
 admin.site.register(models.Category, MPTTModelAdmin)
 admin.site.register(models.Tag)
-admin.site.register(models.Post)
-admin.site.register(models.Recipe)
-admin.site.register(models.Comment)
